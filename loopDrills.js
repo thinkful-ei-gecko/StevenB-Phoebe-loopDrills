1a621c551092366ed72d5c07b46e6cc9c645f1cf
Steven Bull <stevenwbulljr@gmail.com>
2019-08-13 11:03:42 -0500

    completed min/max drill

--- loopDrills.js
+++ loopDrills.js
@@ -1,1 +1,27 @@
 'use strict';
+
+function max(numbers) {
+    let i = 0;
+    let maxNumber = null;
+    while(i < numbers.length) {
+        if (numbers[i] > maxNumber) {
+            maxNumber = numbers[i];
+        }
+        i++;
+    }
+    return maxNumber;
+}
+
+function min(numbers) {
+    let i = 0;
+    let minNumber = numbers[0];
+    while(i < numbers.length) {
+        if (numbers[i] < minNumber) {
+            minNumber = numbers[i];
+        }
+        i++;
+    }
+    return minNumber;
+}
+
+console.log(min([]));
